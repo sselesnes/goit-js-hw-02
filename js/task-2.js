a94afe65b3d5600e316a39adcee09910e0b67444
Yuri Boblak <sseslesnes@gmail.com>
2025-01-22 18:10:47 +0200

    all done but

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,8 @@
-// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
-
-// Доповни код функції таким чином, що:
-
-// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
-// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
-
-function formatMessage(message, maxLength) {}
+function formatMessage(message, maxLength) {
+  return message.length > maxLength
+    ? (message = message.slice(0, maxLength) + "...")
+    : message;
+}
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
-// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
-
-// Доповни код функції таким чином, що:
-
-// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
-// Якщо в рядку відсутні заборонені слова, функція повертає буль false
-
-function checkForSpam(message) {}
+function checkForSpam(message) {
+  return (
+    message.toLowerCase().includes(`spam`) ||
+    message.toLowerCase().includes(`sale`)
+  );
+}
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
